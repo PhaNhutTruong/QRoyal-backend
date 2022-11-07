1f237d8de22a90decbee9d8f5391b397bcd86dd3
PNT <56348901+PhaNhutTruong@users.noreply.github.com>
2022-11-07 18:39:50 +0700

    set timeout

--- src/main/java/com/hard/qroyal/integration/controllers/client/order/OrderController.java
+++ src/main/java/com/hard/qroyal/integration/controllers/client/order/OrderController.java
@@ -103,7 +103,7 @@ public class OrderController extends BaseController<OrderService, OrderMapper> {
 		vnp_Params.put("vnp_Locale", "vn");
 		vnp_Params.put("vnp_ReturnUrl", createOrderRequest.getPaymentRequest().getReturnUrl());
 		vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
-		Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+		Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
 		String vnp_CreateDate = formatter.format(cld.getTime());
 		vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
